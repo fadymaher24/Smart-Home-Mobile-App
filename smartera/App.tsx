d123535324e7a9210d79b813173bc09e0767efc9
fadymaher24 <fady.maher2024@gmail.com>
2025-03-29 05:29:18 +0200

    Refactor App component to display "Hello Halzem" and update styles; add project configuration to app.json and update package dependencies

--- smartera/App.tsx
+++ smartera/App.tsx
@@ -3,22 +3,22 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-  let x = 1;
-  console.log("App executed");
+	let x = 1;
+	console.log("App executed");
 
-  return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<Text>Hello Halzem</Text>
+			<StatusBar style="auto" />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "gainsboro",
+		alignItems: "center",
+		justifyContent: "center",
+	},
 });
