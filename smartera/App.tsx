9f815022ff964658409edbbdde8030113bc3da92
fadymaher24 <fady.maher2024@gmail.com>
2025-03-29 20:03:18 +0200

    Fix syntax error in font loading path for Roboto-Bold in App component

--- smartera/App.tsx
+++ smartera/App.tsx
@@ -7,7 +7,7 @@ import * as Font from "expo-font";
 const getFonts = () => {
 	return Font.loadAsync({
 		"Roboto-Black": require("./assets/fonts/Roboto-Black.ttf"),
-		"Roboto-Bold": `require("./assets/fonts/Roboto-Bold.ttf"),
+		"Roboto-Bold": require("./assets/fonts/Roboto-Bold.ttf"),
 		"Roboto-Light": require("./assets/fonts/Roboto-Light.ttf"),
 		"Roboto-Medium": require("./assets/fonts/Roboto-Medium.ttf"),
 		"Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
