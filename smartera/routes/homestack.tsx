fc5987055d331d688b1fc0329c4c141ebee4b2d0
fadymaher24 <fady.maher2024@gmail.com>
2025-03-30 03:14:08 +0200

    Refactor navigation structure; replace static navigation with native stack navigator in homestack.tsx

--- smartera/routes/homestack.tsx
+++ smartera/routes/homestack.tsx
@@ -4,18 +4,32 @@ import * as React from "react";
 import { View, Text } from "react-native";
 import { createStaticNavigation } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+
+const Stack = createNativeStackNavigator();
 
 import WelcomeScreen from "../app/screens/WelcomeScreen";
 import DeviceControlScreen from "../app/screens/DeviceControlScreen";
 
-const RootStack = createNativeStackNavigator({
-	screens: {
-		Home: WelcomeScreen,
-	},
-});
+function RootStack() {
+	return (
+		<Stack.Navigator initialRouteName="Home">
+			<Stack.Screen name="WelcomeScreen" component={WelcomeScreen} />
+			<Stack.Screen
+				name="DeviceControlScreen"
+				component={DeviceControlScreen}
+			/>
+		</Stack.Navigator>
+	);
+}
 
-const Navigation = createStaticNavigation(RootStack);
-
+function Navigation() {
+	return (
+		<NavigationContainer>
+			<RootStack />
+		</NavigationContainer>
+	);
+}
 export default function App() {
 	return <Navigation />;
 }
