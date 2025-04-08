503b56ea0d67729605117cb70ce486315debd269
fadymaher24 <fady.maher2024@gmail.com>
2025-04-08 23:43:01 +0200

    Refactor navigation structure; remove old homestack.tsx and replace with new navigation implementation in homestack.txt. Delete unused .env copy file and update package.json dependencies.

--- smartera/App.tsx
+++ smartera/App.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react-native";
 import { StyleSheet, Text, View } from "react-native";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
-import Navigator from "./routes/homestack";
+// import Navigator from "./routes/homestack";
 import * as Font from "expo-font";
 
 export default function App(): JSX.Element {
